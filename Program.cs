d16fbbc20e5a5ae85676e85f5fb0733d91f122b5
pedro Okuma <pedrookuma3@gmail.com>
2020-11-17 23:52:35 -0300

    Tarefa tabuada realizada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deseva ver a tabuada de qual número? ");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+            }
+        }
+    }
+}
